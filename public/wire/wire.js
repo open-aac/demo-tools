aece1bd17f37d79a7e806ccbc8e99229a6d7e2ae
Brian Whitmer <brian.whitmer@gmail.com>
2021-03-25 12:54:48 -0600

    update wire wiip

--- public/wire/wire.js
+++ public/wire/wire.js
@@ -10,7 +10,7 @@ var wire = {};
         canvas: canvas,
         context: canvas.getContext('2d')
       };
-      ['ss', 'pcs', 'ara', 'mul', 'taw'].forEach(function(key) {
+      ['ss', 'pcs', 'ara', 'mul', 'taw', 'hc'].forEach(function(key) {
         var canvas = document.getElementById('sample_' + key);
         canvas.width = 400;
         canvas.height = 400;
@@ -56,6 +56,9 @@ var wire = {};
           wire.pics[key].shirt = '#fff';
           wire.pics[key].long_sleeves = true;
           wire.pics[key].shoes = '#000';
+        } else if(key == 'hc') {
+          wire.pics[key].hc = '#f080ac';
+          wire.pics[key].hair = true;
         }
       });
       wire.body = {
@@ -188,15 +191,19 @@ var wire = {};
           var context = pic.context;
           if(context) {
             context.clearRect(0, 0, 400, 400);
-            context.fillStyle = '#eee';
+            context.fillStyle = pic.hc || '#eee';
             context.lineWidth = 5;
             context.lineJoin = 'round';
+            context.lineCap = 'round';
 
             if(pic.wire) {
+              // neck
               context.beginPath();
-              context.moveTo(wire.body.head[0], wire.body.head[1]);
+              context.moveTo(wire.body.head[0], wire.body.head[1] + (pic.head_y || 30));
               context.lineTo(wire.body.chest[0], wire.body.chest[1]);
               context.stroke();
+
+              // arms
               context.beginPath();
               var rshoulder = pic.shoulders == false ? wire.body.chest : wire.body.rshoulder;
               var lshoulder = pic.shoulders == false ? wire.body.chest : wire.body.lshoulder;
@@ -212,11 +219,13 @@ var wire = {};
               context.lineTo(wire.body.lwrist[0], wire.body.lwrist[1]);
               context.stroke();
 
+              // spine
               context.beginPath();
               context.moveTo(wire.body.chest[0], wire.body.chest[1]);
               context.lineTo(wire.body.waist[0], wire.body.waist[1]);
               context.stroke();
 
+              // legs
               context.beginPath();
               context.moveTo(wire.body.waist[0], wire.body.waist[1]);
               context.lineTo(wire.body.rknee[0], wire.body.rknee[1]);
@@ -233,12 +242,12 @@ var wire = {};
             } else {
               if(pic.neck !== false) {
                 // neck
-                context.fillStyle = pic.skin || '#eee';
+                context.fillStyle = pic.hc || pic.skin || '#eee';
                 context.beginPath();
-                context.moveTo(wire.body.head[0] - 5, wire.body.head[1]);
+                context.moveTo(wire.body.head[0] - 5, wire.body.head[1] + (pic.head_y || 30));
                 context.lineTo(wire.body.chest[0] - 5, wire.body.chest[1]);
                 context.lineTo(wire.body.chest[0] + 5, wire.body.chest[1]);
-                context.lineTo(wire.body.head[0] + 5, wire.body.head[1]);
+                context.lineTo(wire.body.head[0] + 5, wire.body.head[1] + (pic.head_y || 30));
                 context.closePath();
                 context.fill();
                 context.stroke();
@@ -250,7 +259,7 @@ var wire = {};
                 // context.stroke();
               }
 
-              context.fillStyle = pic.skin || '#eee';
+              context.fillStyle = pic.hc || pic.skin || '#eee';
               var diff = (pic.arm_thickness || 8) / 2;
               // right arm
               context.beginPath();
@@ -279,7 +288,7 @@ var wire = {};
               context.stroke();
 
               // shirt
-              context.fillStyle = pic.shirt || '#eee';
+              context.fillStyle = pic.hc || pic.shirt || '#eee';
               context.beginPath();
               context.moveTo(wire.body.chest[0], wire.body.chest[1]);
               context.lineTo(wire.body.rshoulder[0] + 5, wire.body.rshoulder[1] - 5);
@@ -289,7 +298,7 @@ var wire = {};
                 context.lineTo(wire.body.rwrist[0] - 10, wire.body.rwrist[1]);
               }
               context.lineTo(wire.body.relbow[0] - 10, wire.body.relbow[1]);
-              context.lineTo(wire.body.rshoulder[0], wire.body.rshoulder[1] + 20);
+              context.lineTo(wire.body.chest[0] + 20, wire.body.chest[1] + 30);
               context.lineTo(wire.body.waist[0] + 20, wire.body.waist[1]);
               if(pic.tunic) {
                 context.lineTo(wire.body.rknee[0] + 10, wire.body.rknee[1]);
@@ -298,7 +307,7 @@ var wire = {};
                 context.lineTo(wire.body.lknee[0] - 10, wire.body.lknee[1]);
               }
               context.lineTo(wire.body.waist[0] - 20, wire.body.waist[1]);
-              context.lineTo(wire.body.lshoulder[0], wire.body.lshoulder[1] + 20);
+              context.lineTo(wire.body.chest[0] - 20, wire.body.chest[1] + 30);
               context.lineTo(wire.body.lelbow[0] + 10, wire.body.lelbow[1]);
               if(pic.long_sleeves) {
                 context.lineTo(wire.body.lwrist[0] + 10, wire.body.lwrist[1]);
@@ -313,16 +322,16 @@ var wire = {};
               if(pic.tunic) {
                 // collar
                 context.beginPath();
-                context.moveTo(wire.body.head[0] -10, wire.body.head[1]);
+                context.moveTo(wire.body.head[0] -10, wire.body.head[1] + (pic.head_y || 30));
                 context.lineTo(wire.body.chest[0] - 10, wire.body.chest[1]);
                 context.lineTo(wire.body.chest[0] + 10, wire.body.chest[1]);
-                context.lineTo(wire.body.head[0] + 10, wire.body.head[1]);
+                context.lineTo(wire.body.head[0] + 10, wire.body.head[1] + (pic.head_y || 30));
                 context.closePath();
                 context.fill();
                 context.stroke();
                 context.beginPath();
-                context.moveTo(wire.body.head[0], wire.body.head[1]);
-                context.lineTo(wire.body.head[0], wire.body.chest[1]);
+                context.moveTo(wire.body.head[0], wire.body.head[1] + (pic.head_y || 30));
+                context.lineTo(wire.body.chest[0], wire.body.chest[1]);
                 context.stroke();
 
                 // cuffs
@@ -345,14 +354,55 @@ var wire = {};
                 context.stroke();
               } else {
                 // pants
-                context.fillStyle = pic.pants || '#eee';
+                context.fillStyle = pic.hc || pic.pants || '#eee';
                 context.beginPath();
                 context.moveTo(wire.body.waist[0], wire.body.waist[1] - 20);
                 context.lineTo(wire.body.waist[0] + 20, wire.body.waist[1] - 20);
-                context.lineTo(wire.body.rknee[0] + 15, wire.body.rknee[1]);
+                var almost_there = function(start, stop) {
+                  var diff_x = stop[0] - start[0];
+                  var diff_y = stop[1] - start[1];
+                  var hyp = Math.sqrt((diff_x * diff_x) + (diff_y * diff_y));
+                  var shrunk = hyp;
+                  if(hyp > 20) {
+                    shrunk = hyp - 10;
+                  } else {
+                    shrunk = hyp * 0.9;
+                  }
+                  var ratio = shrunk / hyp;
+                  return [start[0] + diff_x * ratio, start[1] + diff_y * ratio];
+                };
+                var trace_tangent = function(a, b, target, dist) {
+                  var diff_x = b[0] - a[0];
+                  var diff_y = b[1] - a[1];
+                  var tan_x = -1  * diff_y;
+                  var tan_y = diff_x;
+                  var hyp = Math.sqrt((tan_x * tan_x) + (tan_y * tan_y));
+                  var ratio = dist / hyp;
+                  // context.save();
+                  // context.fillStyle = '#f00';
+                  // context.ellipse(a[0], a[1], 3, 3, 0, 0, 2*Math.PI);
+                  // context.fill();
+                  // context.ellipse(b[0], b[1], 3, 3, 0, 0, 2*Math.PI);
+                  // context.fill();
+                  // context.restore();
+                  return [target[0] + tan_x * ratio, target[1] + tan_y * ratio];
+                };
+
+                var ka = trace_tangent([wire.body.waist[0] + 10, wire.body.waist[1] - 20], wire.body.rknee, wire.body.rknee, -5);
+                var kc = trace_tangent([wire.body.waist[0] + 10, wire.body.waist[1] - 20], wire.body.rknee, wire.body.rknee, 5);
+                var before_knee = almost_there([wire.body.waist[0] + 10, wire.body.waist[1] - 20], ka);
+                var kb = trace_tangent(wire.body.rheel, wire.body.rknee, wire.body.rknee, 5);
+                var kd = trace_tangent(wire.body.rheel, wire.body.rknee, wire.body.rknee, -5);
+                var after_knee = almost_there(wire.body.rheel, kb);
+                var knee_point = wire.body.rknee;
+                context.lineTo(before_knee[0] + 10, before_knee[1]);
+                context.quadraticCurveTo(knee_point[0] + 15, knee_point[1], after_knee[0] + 10, after_knee[1]);
                 context.lineTo(wire.body.rheel[0] + 10, wire.body.rheel[1] - 5);
                 context.lineTo(wire.body.rheel[0] - 10, wire.body.rheel[1] - 5);
-                context.lineTo(wire.body.rknee[0] - 5, wire.body.rknee[1]);
+                var knee_crook = trace_tangent([wire.body.waist[0] + 20, wire.body.waist[1] - 10], before_knee, wire.body.rknee, 10);
+                // var knee_crookb = trace_tangent(wire.body.rheel, after_knee, wire.body.rknee, 125);
+                var knee_crook = [(kc[0] + kd[0]) / 2, (kc[1] + kd[1]) / 2];
+                context.lineTo(knee_crook[0], knee_crook[1]);
                 context.lineTo(wire.body.waist[0], wire.body.waist[1] + 20);
                 context.lineTo(wire.body.lknee[0] + 5, wire.body.lknee[1]);
                 context.lineTo(wire.body.lheel[0] + 10, wire.body.lheel[1] - 5);
@@ -365,7 +415,7 @@ var wire = {};
               }
 
               // right shoe
-              context.fillStyle = pic.shoes || '#eee';
+              context.fillStyle = pic.hc || pic.shoes || '#eee';
               context.beginPath();
               context.moveTo(wire.body.rheel[0] - 10, wire.body.rheel[1] - 5);
               context.lineTo(wire.body.rheel[0] + 10, wire.body.rheel[1] - 5);
@@ -388,15 +438,15 @@ var wire = {};
 
             if(pic.hands  == 'oval') {
               context.fillStyle = '#000';
-              context.beginPath();
-              context.ellipse((wire.body.lheel[0] + wire.body.ltoes[0])/2, (wire.body.lheel[1] + wire.body.ltoes[1])/2, Math.abs(wire.body.lheel[0] - wire.body.ltoes[0])/2, 3, 0, 0, 2 *  Math.PI);
-              context.fill();
-              context.stroke();
+              // context.beginPath();
+              // context.ellipse((wire.body.lheel[0] + wire.body.ltoes[0])/2, (wire.body.lheel[1] + wire.body.ltoes[1])/2, Math.abs(wire.body.lheel[0] - wire.body.ltoes[0])/2, 3, 0, 0, 2 *  Math.PI);
+              // context.fill();
+              // context.stroke();
   
-              context.beginPath();
-              context.ellipse((wire.body.rheel[0] + wire.body.rtoes[0])/2, (wire.body.rheel[1] + wire.body.rtoes[1])/2, Math.abs(wire.body.rheel[0] - wire.body.rtoes[0])/2, 3, 0, 0, 2 *  Math.PI);
-              context.fill();
-              context.stroke();
+              // context.beginPath();
+              // context.ellipse((wire.body.rheel[0] + wire.body.rtoes[0])/2, (wire.body.rheel[1] + wire.body.rtoes[1])/2, Math.abs(wire.body.rheel[0] - wire.body.rtoes[0])/2, 3, 0, 0, 2 *  Math.PI);
+              // context.fill();
+              // context.stroke();
   
               context.beginPath();
               context.ellipse(wire.body.lwrist[0], wire.body.lwrist[1], 3, 8, 0, 0, 2 *  Math.PI);
@@ -409,7 +459,7 @@ var wire = {};
               context.stroke();  
             }
 
-            context.fillStyle = pic.skin || '#eee';
+            context.fillStyle = pic.hc || pic.skin || '#eee';
             context.beginPath();
             var head_y = pic.neck == false ? wire.body.head[1] + 15 : wire.body.head[1];
             if(pic.head == 'bottom-heavy') {
@@ -418,14 +468,22 @@ var wire = {};
               context.quadraticCurveTo(wire.body.head[0] + (pic.head_x / 2), head_y - pic.head_y, wire.body.head[0], head_y - pic.head_y);
               context.quadraticCurveTo(wire.body.head[0] - (pic.head_x / 2), head_y - pic.head_y, wire.body.head[0] - pic.head_x, head_y);
               context.quadraticCurveTo(wire.body.head[0] - (pic.head_x * 1.3), head_y + pic.head_y, wire.body.head[0] - 5, head_y + pic.head_y);
+              context.fill();
+              context.stroke();
             } else {
               context.ellipse(wire.body.head[0], head_y, pic.head_x || 30, pic.head_y || 30, 0, 0, 2 *  Math.PI);
+              context.fill();
+              context.stroke();
+              if(pic.hair) {
+                context.fillStyle = pic.hc || pic.hair || '#a1532c';
+                context.beginPath();
+                context.ellipse(wire.body.head[0], head_y, pic.head_x || 30, pic.head_y || 30, 0, Math.PI, 2 *  Math.PI);
+                context.quadraticCurveTo(wire.body.head[0] + (pic.head_x || 30), wire.body.head[1] - (pic.head_y || 30) / 2, wire.body.head[0] + (pic.head_x || 30) / 3, head_y - (pic.head_y || 30) / 3);
+                context.quadraticCurveTo(wire.body.head[0], wire.body.head[1], wire.body.head[0] - (pic.head_x || 30), head_y);
+                context.fill();
+                context.stroke();
+              }
             }
-            if(pic.hair) {
-
-            }
-            context.fill();
-            context.stroke();
             
           }
         }
