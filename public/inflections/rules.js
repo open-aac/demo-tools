a446d1178eb9278948ac9065ab5d64ad117382c5
Brian Whitmer <brian.whitmer@gmail.com>
2024-09-23 15:20:10 -0600

    support csv for words and rules lists

--- public/inflections/rules.js
+++ public/inflections/rules.js
@@ -1,24 +1,36 @@
+/*
+  Usage:
+  await in7ns.load('en', {words_url: "http://...", rules_url: "http://CORS-SAFE-URL"});
+  in7ns.lookup("he", null, 'en');
+  in7ns.lookup("he", "I like", 'en');
+*/
 var in7ns = {};
 (function() {
+  var locale_sets = {};
+
+  // matches_rule and lookup are the two internal methods that
+  // encapsulate the inflection rules algorithms. We document
+  // them here to make it easier to implement in other languages.
+
+  // matches_rule is called internally by lookup as a reusable
+  // way to check whether the current button state matches
+  // the requirements for a given rule
   var matches_rule = function(rule, buttons, do_debug) {
     if(rule.id == 'xxx' && do_debug) { debugger }
+    // Keep an index for the currently-considered prior button
     var history_idx = buttons.length - 1;
     var valid = true;
     var condenses = [];
+    // Working backwards, iterate through all the lookback rules
+    // one at a time
     for(var idx = rule.lookback.length - 1; idx >= 0 && valid; idx--) {
+      // Retrieve the current prior button, current check, and check before the current check 
       var item = buttons[history_idx]
       var check = rule.lookback[idx];
       var pre_check = rule.lookback[idx - 1]
-      if(typeof(check) == 'string') {
-        if(check.match(/^\(/)) {
-          check = {type: check.replace(/\(|\)/g, '')};
-        } else if(check.match(/^\[/)) {
-          check = {words: check.replace(/^\[/, '').replace(/\[$/, '').split(/|/).map(function(w) { return w.replace(/^\s+/, '').replace(/\s+$/, ''); })};
-        } else {
-          check = {words: [check]};
-        }
-      }
       if(!item) { 
+        // If the current rule is not optional and there are
+        // no items left to look back at, then the rule is unsatisfied
         if(!check.optional) {
           valid = false;
         }
@@ -26,15 +38,21 @@ var in7ns = {};
         var label = item.word.toLowerCase();
         var matching = false;
         var pre_matching = false;
+        // If the currently-considered button matches the current check's
+        // word list then rule is still satisfied for now
         if(check.words) {
           if(check.words.indexOf(label) != -1) {
             matching = true;
           }
+        // Likewise, if the currently-considered button matched
+        // the current check's part of speech then the rule is satisfied
+        // for now
         } else if(check.type) {
           if((item.types || []).indexOf(check.type) != -1) {
             matching = true;
           }
         }
+        // Perform the same checks on the check before the current check
         if(matching && pre_check) {
           if(pre_check.words) {
             if(pre_check.words.indexOf(label) != -1) {
@@ -47,50 +65,81 @@ var in7ns = {};
           }
         }
         if(matching) {
+          // This is the tricky bit. If the current check also works
+          // for the check before the current check, and the current check
+          // is option but the one before isn't, then this button
+          // should be used to satisfy the check before instead of the 
+          // current one. Note there are potentially more edge cases
+          // that could be considered, but so far this seems sufficient.
           if(check.optional && pre_matching && !pre_check.optional) {
             matching = false
-          } else if(check.match) {
+          }
+          // If the current check requires a regex match that the
+          // button doesn't match, then this button doesn't satisfy
+          if(check.match) {
             if(!label.match(check.match)) {
               matching = false;
             }
           }
+          // If the current check requires a regex non-match that the
+          // button does match, then this button doesn't satisfy
           if(check.non_match) {
             if(label.match(check.non_match)) {
               matching = false;
             }
           }  
         }
+        // If the current button matches, then the rule is
+        // satisfactory so far, and checks may continue
         if(matching) {
+          // If the current button is used for the current
+          // check, and is marked consense, then this index
+          // into the prior button list needs to be marked
+          // as to-condense if this result is selected
           if(check.condense) {
             condenses.push(history_idx);
           }
           history_idx--;
+        // If not matching and the check isn't optional, then
+        // the rule cannot be satisfied by the button sequence
         } else if(!check.optional) {
           valid = false;
         }
       }
     }
+    // If condense indexes need to be remembered, this
+    // information is returned instead of a boolean as 
+    // additional information needed in the final result
     if(valid && condenses.length > 0) {
       return {condense_items: condenses};
     }
     return !!valid;
-  }
-  var lookup = function(prior_txt, word_string, do_debug) {
+  };
+
+  // lookup is called basically externally, and expects
+  // either a word or string of prior words (or both) which
+  // can be used to find matching rules and words. The return
+  // value is a hash with attributes 'words' and 'rules' defined
+  var lookup = function(prior_txt, word_string, locale, do_debug) {
+    if(!locale || !word_string) { return {error: 'missing word or locale'}; }
+    var settings = locale_sets[locale] || locale_sets[locale.split(/-|_/)[0]];
+    if(!settings) { return {error: 'locale ' + locale + ' no loaded' }; }
     // Rule checking is greedy, finding the first matching rule for each type
     var found_types = {};
     var matching_rules = [];
+    // Map the prior sentence string to a list of word objects
     var parts = prior_txt.split(/\s+/);
     var prior_buttons = [];
     parts.forEach(function(str) {
       if(str) {
-        var wrd = words.find(function(w) { return w.word == str; });
+        var wrd = settings.words.find(function(w) { return w.word == str; });
         prior_buttons.push(wrd || {
           word: str
         });  
       }
     });
-    var found_words = words.filter(function(w) { return w.word == word_string; });
-    rules.forEach(function(rule) {
+    var found_words = settings.words.filter(function(w) { return w.word == word_string; });
+    settings.rules.forEach(function(rule) {
       if(prior_buttons.length == 0) {
         var matching_replacement = null;
         if(rule.type == 'override') {
@@ -98,9 +147,9 @@ var in7ns = {};
         } else if(found_words.find(function(w) { return w.types.indexOf(rule.type) != -1; })) {
           var word = found_words.find(function(w) { return w.types.indexOf(rule.type) != -1; })
           if(rule.inflection && word && word.inflections) {
-            word.types.forEach(function(t) { 
+            // word.types.forEach(function(t) { 
               matching_replacement = matching_replacement || word.inflections[rule.inflection];
-            });
+            // });
           }
         }
         if(matching_replacement) {
@@ -111,7 +160,7 @@ var in7ns = {};
             if(l.words) {
               pre_word = l.words[Math.floor(Math.random() * l.words.length)];
             } else if(l.type) {
-              var type_words = words.filter(function(w) { return w.types[0] == key; });
+              var type_words = settings.words.filter(function(w) { return w.types[0] == key; });
               pre_word = type_words[Math.floor(Math.random() * type_words.length)];
             }
             if(pre_word && (!l.optional || Math.random() > 0.5)) {
@@ -186,7 +235,7 @@ var in7ns = {};
       });
     }
     return res;
-  }
+  };
 
   function parseCSV(str) {
     const arr = [];
@@ -229,52 +278,86 @@ var in7ns = {};
     for(var idx = 0; idx < arr[0].length; idx++) {
       head[idx] = arr[0][idx];
     }
-    var words = {};
+    var results = {};
+    var last = null;
     for(var idx = 1; idx < arr.length; idx++) {
       var row = {};
-      var word = null;
+      var id = null;
       for(var jdx = 0; jdx < arr[idx].length; jdx++) {
         var val = arr[idx][jdx];
         if(jdx == 0) {
-          word = arr[idx][jdx]
+          id = arr[idx][jdx];
         } else {
           if(val && val != 'NA' && val != 'N/A') {
             row[head[jdx]] = arr[idx][jdx];
-          }  
+          }
         }
       }
-      if(word && Object.keys(row).length > 0) {
-        words[word] = row;
+      if(id && Object.keys(row).length > 0) {
+        results[id] = row;
+        last = row;
+      } else if(!id && Object.keys(row).length > 0) {
+        last.subrows = last.subrows || [];
+        last.subrows.push(row);
       }
     }
-    return words;
+    return results;
   };
 
-  in7ns.load = function(locale) {
+  in7ns.load = function(locale, opts) {
+    opts = opts || {};
+    var fallback_locale = locale.split(/-|_/)[0];
     return new Promise(function(resolve, reject) {
-      fetch("./words-" + locale + ".json").then(function(res) { 
-        var words = null;
+      var words_url = opts.words_url || "./words-" + locale + ".json";
+      var rules_url = opts.rules_url || "./rules-" + locale + ".json";
+      fetch(words_url).then(function(res) { 
         res.json().then(function(json) {
           var words = json;
-          fetch("./rules-" + locale + ".json").then(function(res) {
+          var words_list = [];
+          for(var key in words) {
+            if(!key.match(/^_/)) {
+              words[key].word = key;
+              words_list.push(words[key]);
+            }
+          }
+          fetch(rules_url).then(function(res) {
             res.json().then(function(json) {
+              var rules_list = json.rules;
+              rules_list.forEach(function(rule) {
+                (rule.lookback || []).forEach(function(lookback) {
+                  if(lookback.non_match) { lookback.nms = lookback.non_match; lookback.non_match = new RegExp(lookback.non_match); };
+                  if(lookback.match) { lookback.ms = lookback.match; lookback.match = new RegExp(lookback.match); };
+                })
+              });
+        
+              locale_sets[locale] = {
+                words: words_list,
+                rules: rules_list
+              };
+              if(fallback_locale != locale || !locale_sets[fallback_locale] || locale_sets[fallback_locale].fallback) {
+                locale_sets[fallback_locale] = {
+                  fallback: true,
+                  words: words_list,
+                  rules: rules_list
+                };
+              }
               resolve({
-                rules: json.rules,
-                words: words,
+                rules: rules_list,
+                words: words_list,
                 tests: json.tests
               });
             }, function(err) {
               reject({
                 error: err,
                 message: "error parsing rules for " + locale,
-                words: words
+                words: words_list
               });
             });
           }, function(err) {
             reject({
               error: err,
               message: "error retrieving rules for " + locale,
-              words: words
+              words: words_list
             });
           });
         }, function(err) {
@@ -291,8 +374,8 @@ var in7ns = {};
       });
     });
   };
-  in7ns.lookup = function(prior_words, lookup_text, locale, do_debug) {
-    return lookup(prior_words, lookup_text, do_debug);
+  in7ns.lookup = function(lookup_word, prior_words, locale, do_debug) {
+    return lookup(prior_words, lookup_word, locale, do_debug);
   }
   in7ns.parse_words = function(str) {
     var hash = csvToHash(str);
@@ -314,5 +397,48 @@ var in7ns = {};
       new_hash[key] = word;
     }
     return new_hash;
+  }
+  in7ns.parse_rules = function(str) {
+    var hash = csvToHash(str);
+    var rules = [];
+    for(var key in hash) {
+      var rule = {id: key};
+      for(var opt in hash[key]) {
+        if(opt == 'subrows') {
+          hash[key][opt].forEach(function(sub) {
+            if(sub.subtype == 'lookback') {
+              rule.lookback = rule.lookback || [];
+              var lookback = {};
+              if(sub.subtype_val2) {
+                lookback.type = sub.subtype_val2;
+              }
+              if(sub.subtype_val1) {
+                lookback.words = sub.subtype_val1.split(/\|/);
+              }
+              if(sub.lookbacks_optional == '1') {
+                lookback.optional = true;
+              }
+              if(sub.lookbacks_match) {
+                lookback.match = sub.lookbacks_match;
+              }
+              if(sub.lookbacks_non_match) {
+                lookback.non_match = sub.lookbacks_non_match;
+              }
+              if(sub.lookbacks_condense == '1') {
+                lookback.condense = true;
+              }
+              rule.lookback.push(lookback);
+            } else if(sub.subtype == 'override') {
+              rule.overrides = rule.overrides || {}
+              rule.overrides[sub.subtype_val1] = sub.subtype_val2;
+            }
+          });
+        } else {
+          rule[opt] = hash[key][opt];
+        }  
+      }
+      rules.push(rule);
+    }
+    return rules;  
   }
 })();
